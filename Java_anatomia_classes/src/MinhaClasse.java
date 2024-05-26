2926e105b693b5d94ef7b12dd75feb7c89378301
HEBERT DE OLIVEIRA PEREIRA <pereira_hebert@msn.com>
2024-05-26 10:38:18 -0300

    classes e metodos

---
+++ Java_anatomia_classes/src/MinhaClasse.java
@@ -0,0 +1,6 @@
+public class MinhaClasse {//corpo da classe com inciais maiusculas
+    public static void main(String[] args) {//medoto principal
+        System.out.println("Olá, mundo!");//imprime na tela
+    }
+    
+}
