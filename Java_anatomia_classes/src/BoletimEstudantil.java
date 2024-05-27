a2aad27b11eadd5b3a505c41ba0d5f39a5ebe952
HEBERT DE OLIVEIRA PEREIRA <pereira_hebert@msn.com>
2024-05-26 21:58:37 -0300

    metodos indentação

---
+++ Java_anatomia_classes/src/BoletimEstudantil.java
@@ -0,0 +1,13 @@
+public class BoletimEstudantil {
+    public static void main(String[] args) {
+        int mediaFinal = 4;
+        if (mediaFinal >= 7) {
+            System.out.println("Aprovado");
+        } else if (mediaFinal >= 5 && mediaFinal < 7) {
+            System.out.println("Recuperação");
+        } else {
+            System.out.println("Reprovado");
+            
+        }
+    }
+}
