7ec09b43f3859cf96c04dc101d990cc87d63af27
HEBERT DE OLIVEIRA PEREIRA <pereira_hebert@msn.com>
2024-05-26 21:34:26 -0300

    sintaxe e variaves

--- Java_anatomia_classes/src/MinhaClasse.java
+++ Java_anatomia_classes/src/MinhaClasse.java
@@ -1,6 +1,16 @@
 public class MinhaClasse {//corpo da classe com inciais maiusculas
     public static void main(String[] args) {//medoto principal
-        System.out.println("Olá, mundo!");//imprime na tela
+        String nome = "Fulano";//variavel do tipo string
+        System.out.println(nome);//imprime a variavel nome
+
+        int idade = 30;//variavel do tipo inteiro
+        System.out.println(idade);//imprime a variavel idade
+
+        double altura = 1.80;//variavel do tipo double]
+        System.out.println(altura);//imprime a variavel altura
+
+        boolean estudante = true;//variavel do tipo boolean
+        System.out.println(estudante);//imprime a variavel estudante
     }
     
 }
