8ad0da103ea1ed2408c851d2d61a7b8772ed92d0
HEBERT DE OLIVEIRA PEREIRA <pereira_hebert@msn.com>
2024-05-27 22:49:16 -0300

    relacionais

--- Java_anatomia_classes/src/est/seg/operadores.java
+++ Java_anatomia_classes/src/est/seg/operadores.java
@@ -69,6 +69,26 @@ public class operadores {
         String resultado = (a1 == b1) ? "verdadeiro" : "falso";
 
         System.out.println(resultado);
+
+        //classe Operadores.java
+int numero1 = 1;
+int numero2 = 2;
+
+if(numero1 > numero2)
+	System.out.print("Numero 1 maior que numero 2\n");
+
+if(numero1 < numero2)
+	System.out.print("Numero 1 menor que numero 2\n");
+
+if(numero1 >= numero2)
+	System.out.print("Numero 1 maior ou igual que numero 2\n");
+
+if(numero1 <= numero2)
+	System.out.print("Numero 1 menor ou igual que numero 2\n");
+
+if(numero1 != numero2)
+	System.out.print("Numero 1 é diferente de numero 2\n");
+
     }
 
 }
