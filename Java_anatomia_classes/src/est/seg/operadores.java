52a6f274beca81da2c20fc1cce9419a6b7fb4037
HEBERT DE OLIVEIRA PEREIRA <pereira_hebert@msn.com>
2024-05-27 22:56:28 -0300

    Expressões lógicas avançadas

--- Java_anatomia_classes/src/est/seg/operadores.java
+++ Java_anatomia_classes/src/est/seg/operadores.java
@@ -89,6 +89,24 @@ if(numero1 <= numero2)
 if(numero1 != numero2)
 	System.out.print("Numero 1 é diferente de numero 2\n");
 
+    // Operadores.java
+boolean condicao1=true;
+
+boolean condicao2=false;
+
+/* Aqui estamos utilizando o operador lógico E para fazer a união de duas 
+expressões. 
+É como se estivesse escrito:
+ "Se Condicao1 e Condicao2 forem verdadeiras, executar código"
+*/
+
+if(condicao1 && condicao2)
+	System.out.print("Os dois valores precisam ser verdadeiros");
+
+//Se condicao1 OU condicao2 for verdadeira, executar código.
+if(condicao1 || condicao2)
+	System.out.print("Um dos valores precisa ser verdadeiro");
+
     }
 
 }
