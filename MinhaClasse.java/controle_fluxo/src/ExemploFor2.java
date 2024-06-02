08f6c441c5b29030cdd8e2186a6d635e79e953c9
HEBERT DE OLIVEIRA PEREIRA <pereira_hebert@msn.com>
2024-06-02 18:33:26 -0300

    For Each

---
+++ MinhaClasse.java/controle_fluxo/src/ExemploFor2.java
@@ -0,0 +1,12 @@
+// ExemploFor.java
+public class ExemploFor2 {
+    public static void main(String[] args) {
+        String alunos [] =  {"FELIPE","JONAS","JULIA","MARCOS"};
+        
+            //Forma abreviada
+        for(String aluno : alunos) {
+          System.out.println(" Nome do aluno é " + aluno);
+        }
+    
+    }
+    }
