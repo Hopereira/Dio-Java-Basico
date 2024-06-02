0b39b64d4cdbcb9a65f674dfe5917cbca911da9e
HEBERT DE OLIVEIRA PEREIRA <pereira_hebert@msn.com>
2024-06-02 16:44:28 -0300

    Condicionais encadeadas

--- MinhaClasse.java/controle_fluxo/src/ResultadoEscolar.java
+++ MinhaClasse.java/controle_fluxo/src/ResultadoEscolar.java
@@ -2,12 +2,13 @@
 public class ResultadoEscolar {
     public static void main(String[] args) {
 
-       int nota = 8;
+       int nota = 5;
        
        if(nota >= 6)
         System.out.println("Aprovado");
-
-       else
+       else if (nota >= 4 && nota < 6)
+        System.out.println("Recuperação");
+        else
         System.out.println("Reprovado");
     }
 }
