d871fe5d4517b08c01b2a4e2c26c4ea507b76ad0
HEBERT DE OLIVEIRA PEREIRA <pereira_hebert@msn.com>
2024-06-02 21:38:02 -0300

    Exceções customizadas

---
+++ MinhaClasse.java/controle_fluxo/src/FormatadordeCepExemplo.java
@@ -0,0 +1,20 @@
+public class FormatadordeCepExemplo {
+    public static void main(String[] args) {
+        try {
+            String cepFormatado = formatarCep("23765064");
+            System.out.println(cepFormatado);
+        } catch (CepInvalidoException e) {
+            // TODO Auto-generated catch block
+            System.out.println("Ocorreu um erro ao formatar o cep");
+            e.printStackTrace();
+        }
+        
+    }
+    static String formatarCep(String cep) throws CepInvalidoException{
+        if(cep.length() != 8)
+          throw new CepInvalidoException();
+        
+          //simulando um cep formatado
+          return "23.765-064";
+    }
+}
